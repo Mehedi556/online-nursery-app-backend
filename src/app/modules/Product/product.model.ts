9b256f6424edbad372ce5e336da81ad0b1e4aa15
Abdus Salam Mehedi <salam.mehedi99@gmail.com>
2024-08-18 18:15:30 +0600

    added product model

---
+++ src/app/modules/Product/product.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+},
+{
+    timestamps: true
+}
+)
+
+export const Product = model<TProduct>('Product', productSchema)
